23c0ebbb0c267385ca2ce4bfde69eaff9264168b
steven.walsh <imannoyed4days@hotmail.co.uk>
2015-05-07 16:22:37 +0100

    characters done

---
+++ public/modules/characters/controllers/characters.client.controller.js
@@ -0,0 +1,250 @@
+'use strict';
+
+// Characters controller
+angular.module('characters').controller('CharactersController', ['$scope', '$stateParams', '$location', 'Authentication', 'Characters', 'Animeitems', 'Mangaitems', 'fileUpload', '$sce', '$window',
+	function($scope, $stateParams, $location, Authentication, Characters, Animeitems, Mangaitems, fileUpload, $sce, $window) {
+		$scope.authentication = Authentication;
+        
+        $scope.sortType = 'name'; //default sort type
+	    $scope.sortReverse = false; // default sort order
+        $scope.imgPath = ''; //image path
+        //$scope.newTag = ''; //for adding tags.
+        $scope.tagArray = []; // holding tags pre-submit
+        $scope.tagArrayRemove = [];
+
+        //allow retreival of local resource
+        $scope.trustAsResourceUrl = function(url) {
+            return $sce.trustAsResourceUrl(url);
+        };
+        
+        //for adding/removing tags.
+        $scope.addTag = function () {
+                $scope.tagArray.push({ text: $scope.newTag });
+                $scope.newTag = '';
+        };
+        $scope.deleteTag = function(text) {
+        
+            var removal = $window.confirm('Are you sure you want to delete this tag?');
+            if (removal) {
+                var deletingItem = $scope.tagArray;
+                $scope.tagArray = [];
+
+                //update the complete task.
+                angular.forEach(deletingItem, function(tag) {
+                    if (tag.text !== text) {
+                        $scope.tagArray.push(tag);
+                    }
+                });
+            }
+        };
+        //remove existing tag.
+        $scope.removeTag = function(tag) {
+            var removal = $window.confirm('Are you sure you want to delete this tag?');
+            if (removal) {
+                var index = $scope.character.tags.indexOf(tag);
+                $scope.character.tags.splice(index, 1);
+            }
+        };
+        
+        //special tag filter
+        $scope.tagFilter = function(item) {
+            var found = false;
+            var i = 0;
+            var tagsToSearch = [];
+            if ($scope.characterTags===undefined || $scope.characterTags==="") {
+                return true;
+            } else {
+                //get tags that are being looked for
+                var tagsForFilter = $scope.characterTags.split(',');
+                
+                //get tags of items to filter
+                angular.forEach(item.tags, function(tag) {
+                    tagsToSearch.push(tag.text);
+                });
+                
+                //filter: check in 'query' is in tags.
+                for(i = 0; i < tagsForFilter.length; i++) {
+                    
+                    if (tagsToSearch.indexOf(tagsForFilter[i]) !== -1) {
+                        found = true;
+                    } else {
+                        return false;
+                    }
+                }
+                return found;
+            }
+        };
+        
+        //special media filter
+        $scope.mediaFilter = function(item) {
+            if ($scope.media==='anime') {
+                if (item.anime!==null && item.manga===null) {
+                    return true;
+                }
+                return false;
+            } else if ($scope.media==='manga') {
+                if (item.manga!==null && item.anime===null) {
+                    return true;
+                }
+                return false;
+            } else if ($scope.media==='both') {
+                if (item.anime!==null && item.manga!==null) {
+                    return true;
+                }
+                return false;
+            } else {
+                return true;
+            }
+        };
+        
+		// Create new Character
+		$scope.create = function() {
+            console.log($scope.tagArray);
+            //Handle situation if objects not selected.
+			if (this.anime!==undefined && this.manga!==undefined && this.anime!==null && this.manga!==null) {
+             // Create new Character object
+			 var character = new Characters ({
+				name: this.name,
+                image: $scope.imgPath,
+                anime: this.anime._id,
+                manga: this.manga._id,
+                voice: this.voice,
+                tags: $scope.tagArray,
+                user: this.user
+			 });
+            } else if (this.anime!==undefined && this.anime!==null) {
+             var character = new Characters ({
+				name: this.name,
+                image: $scope.imgPath,
+                anime: this.anime._id,
+                manga: this.manga,
+                voice: this.voice,
+                tags: $scope.tagArray,
+                user: this.user
+			 });
+            } else if (this.manga!==undefined && this.manga!==null) {
+             var character = new Characters ({
+				name: this.name,
+                image: $scope.imgPath,
+                anime: this.anime,
+                manga: this.manga._id,
+                voice: this.voice,
+                tags: $scope.tagArray,
+                user: this.user
+			 });
+            } else {
+             var character = new Characters ({
+				name: this.name,
+                image: $scope.imgPath,
+                anime: this.anime,
+                manga: this.manga,
+                voice: this.voice,
+                tags: $scope.tagArray,
+                user: this.user
+			 });
+            }
+            
+			// Redirect after save
+			character.$save(function(response) {
+				$location.path('characters/' + response._id);
+
+				// Clear form fields
+				$scope.name = '';
+                $scope.image = '';
+                $scope.voice = '';
+                $scope.tags = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Remove existing Character
+		$scope.remove = function(character) {
+            var removal = $window.confirm('Are you sure you want to delete this item?');
+            if (removal) {
+			 if ( character ) { 
+				character.$remove();
+
+				for (var i in $scope.characters) {
+					if ($scope.characters [i] === character) {
+						$scope.characters.splice(i, 1);
+					}
+				}
+			 } else {
+				$scope.character.$remove(function() {
+					$location.path('characters');
+				});
+			 }
+            }
+		};
+
+		// Update existing Character
+		$scope.update = function() {
+			var character = $scope.character;
+            if ($scope.tagArray!==undefined) {
+                var temp = character.tags ;
+                character.tags = temp.concat($scope.tagArray);
+            }
+            
+            if ($scope.imgPath!==undefined && $scope.imgPath!==null && $scope.imgPath!=='') {
+                character.image = $scope.imgPath;
+            }
+            
+            
+			character.$update(function() {
+				$location.path('characters/' + character._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Find a list of Characters
+		$scope.find = function() {
+			$scope.characters = Characters.query();
+            console.log($scope.characters);
+		};
+
+		// Find existing Character
+		$scope.findOne = function() {
+			$scope.character = Characters.get({ 
+				characterId: $stateParams.characterId
+			});
+            console.log($scope.character);
+		};
+        
+        // Find a list of Animeitems
+		$scope.findAnime = function() {
+			$scope.animeitems = Animeitems.query();
+		};
+        
+        // Find existing Animeitem
+		$scope.findOneAnime = function(anime) {
+            console.log(anime);
+			$scope.animeitem = Animeitems.get({ 
+				animeitemId: anime
+			});
+		};
+        
+        // Find a list of Mangaitems
+		$scope.findManga = function() {
+			$scope.mangaitems = Mangaitems.query();
+		};
+        
+        // Find existing Animeitem
+		$scope.findOneManga = function(manga) {
+			$scope.mangaitem = Mangaitems.get({ 
+				mangaitemId: manga
+			});
+		};
+        
+        
+        // Image upload
+        $scope.uploadFile = function(){
+            var file = $scope.myFile;
+            $scope.imgPath = '/modules/characters/img/' + file.name;
+            console.log('file is ' + JSON.stringify(file));
+            var uploadUrl = '/fileUploadCharacter';
+            fileUpload.uploadFileToUrl(file, uploadUrl);
+        };
+	}
+]);

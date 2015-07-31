68bfe0255d73f1bc07c79c29681cd413c2842265
swalsh <imannoyed4days@hotmail.co.uk>
2015-07-31 12:47:56 +0100

    improved visiblity of most theme text colours. added option to set theme for time of day gimmick

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -23,6 +23,10 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
         $scope.theme = (localStorage.getItem('theme')!==null) ? JSON.parse($scope.saved) : 'style/main.min.css';
         localStorage.setItem('theme', JSON.stringify($scope.theme));
         
+        $scope.isTimedTheme = localStorage.getItem('timedTheme');
+        $scope.timedTheme = (localStorage.getItem('timedTheme')!==null) ? JSON.parse($scope.isTimedTheme) : false;
+        localStorage.setItem('timedTheme', JSON.stringify($scope.timedTheme));
+        
         //user-selected style options/defaults.
         $scope.styles = [
             { name: 'Blue', url: 'style/main.min.css' },
@@ -33,10 +37,26 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
         ];
         
         $scope.changeTheme = function() {
-            localStorage.setItem('theme', JSON.stringify($scope.theme));
+            localStorage.setItem('timedTheme', JSON.stringify($scope.timedTheme));
+            var timeOfDayTheme = localStorage.getItem('timedTheme');
+            if (timeOfDayTheme === 'false') {
+                localStorage.setItem('theme', JSON.stringify($scope.theme));
+            } else {
+                var time = new Date().getHours();
+                if (time > 20 || time < 6) {
+                    localStorage.setItem('theme', JSON.stringify('style/main-night.min.css'));
+                } else if (time > 17) {
+                    localStorage.setItem('theme', JSON.stringify('style/main-purple.min.css'));
+                } else if (time > 9) {
+                    localStorage.setItem('theme', JSON.stringify('style/main-day.min.css'));
+                } else if (time > 6) {
+                    localStorage.setItem('theme', JSON.stringify('style/main.min.css'));
+                }
+            }
             var storedValue = localStorage.getItem('theme'),
-                link = document.getElementById('app-theme');
-                link.href = storedValue.substr(1, storedValue.lastIndexOf('\"') - 1); //remove quotes for whatever reason.
+            link = document.getElementById('app-theme');
+            link.href = storedValue.substr(1, storedValue.lastIndexOf('\"') - 1); //remove quotes for whatever reason.
+            $scope.theme = storedValue.substr(1, storedValue.lastIndexOf('\"') - 1); //remove quotes for whatever reason. //set the dropdown to the correct value;
         };
         
 	}

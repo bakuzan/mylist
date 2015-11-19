ce10f84a5b6ae85473f7e6dd361e5d7f1536f91b
swalsh <imannoyed4days@hotmail.co.uk>
2015-11-19 16:30:26 +0000

    fixed alternate stylesheet issue.

--- gruntfile.js
+++ gruntfile.js
@@ -167,11 +167,11 @@ module.exports = function(grunt) {
 		      compass: false
 		    },
 		    files: {
-		      'public/dist/main.min.css': 'public/style/main.scss',
-              'public/dist/main-red.min.css': 'public/style/main-red.scss',
-              'public/dist/main-purple.min.css': 'public/style/main-purple.scss',
-              'public/dist/main-day.min.css': 'public/style/main-day.scss',
-              'public/dist/main-night.min.css': 'public/style/main-night.scss'
+		      'public/style/main.min.css': 'public/style/main.scss',
+              'public/style/main-red.min.css': 'public/style/main-red.scss',
+              'public/style/main-purple.min.css': 'public/style/main-purple.scss',
+              'public/style/main-day.min.css': 'public/style/main-day.scss',
+              'public/style/main-night.min.css': 'public/style/main-night.scss'
 		    }
 		  }
         }

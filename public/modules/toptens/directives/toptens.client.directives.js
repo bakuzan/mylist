3b3463179df39112d6c1f587f1a7b5cd01785177
swalsh <imannoyed4days@hotmail.co.uk>
2016-01-25 09:42:53 +0000

    created sticky top directive. working on toptens

--- public/modules/toptens/directives/toptens.client.directives.js
+++ public/modules/toptens/directives/toptens.client.directives.js
@@ -21,9 +21,10 @@ angular.module('toptens')
       templateUrl: '/modules/toptens/templates/step.html',
       link: function(scope, elm, attrs) {
           scope.stepNumber = elm.index() + 1;
+          var element = document.getElementById('step-'+scope.stepNumber);
+          
           scope.$watch('stepConfig.currentStep', function(newValue) {
               if (newValue !== undefined) {
-                  var element = document.getElementById('step-'+scope.stepNumber);
                   if (scope.stepNumber === scope.stepConfig.currentStep) {
                       element.classList.remove('step-transition');
                       element.classList.remove('step-out');
@@ -50,4 +51,36 @@ angular.module('toptens')
        template: '<div class="step-controls form-group step-button-group padding-5" ng-transclude>' +
                   '</div>'
    };
+})
+.directive('sticky', function() {
+    return {
+        restrict: 'A',
+        scope: {},
+        link: function(scope, element, attrs) {
+            
+            window.addEventListener('scroll', function (evt) {
+                var stickyClass = 'sticky-scroll-top', stickyInnerClass = 'sticky-inner-container',
+                    scrollTop = document.body.scrollTop,
+                    elm = element[0],
+                    inner = elm.children[0],
+                    viewportOffset = elm.getBoundingClientRect(),
+                    distance_from_top = viewportOffset.top; // This value is your scroll distance from the top
+
+                // The element has scrolled to the top of the page. Set appropriate style.
+                if (distance_from_top < 56) {
+//                    console.log('top hit : ', distance_from_top);
+                    elm.classList.add(stickyClass);
+                    inner.classList.add(stickyInnerClass);
+                }
+
+                // The element has not reached the top.
+                if(distance_from_top > 55 || scrollTop < 10) {
+//                    console.log('we are at: ', distance_from_top);
+                    elm.classList.remove(stickyClass);
+                    inner.classList.remove(stickyInnerClass);
+                }
+          });
+            
+        }
+    };
 });

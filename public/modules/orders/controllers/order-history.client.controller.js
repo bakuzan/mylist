298bd95fa3bc510ead4b9a23d99085ebbb6ea11d
swalsh <imannoyed4days@hotmail.co.uk>
2016-03-26 21:26:14 +0000

    order history working.

--- public/modules/orders/controllers/order-history.client.controller.js
+++ public/modules/orders/controllers/order-history.client.controller.js
@@ -1,15 +1,27 @@
 'use strict';
 var OrderHistoryController = (function () {
-    function OrderHistoryController($scope, $uibModalInstance, order) {
+    function OrderHistoryController($scope, $uibModalInstance, order, $filter) {
         this.$scope = $scope;
         this.$uibModalInstance = $uibModalInstance;
         this.order = order;
+        this.$filter = $filter;
     }
+    OrderHistoryController.prototype.amountPaid = function (prices) {
+        var _this = this;
+        angular.forEach(prices, function (price) {
+            console.log(price, price.paid);
+            if (price.paid) {
+                var cost = _this.$filter('number')(price.price, 2);
+                cost = _this.$filter('currency')(price.price, '£');
+                return cost;
+            }
+        });
+    };
     OrderHistoryController.prototype.cancel = function () {
         this.$uibModalInstance.dismiss('cancel');
     };
     OrderHistoryController.controllerId = 'OrderHistoryController';
-    OrderHistoryController.$inject = ['$scope', '$uibModalInstance', 'order'];
+    OrderHistoryController.$inject = ['$scope', '$uibModalInstance', 'order', '$filter'];
     return OrderHistoryController;
 }());
 angular.module('orders').controller(OrderHistoryController.controllerId, OrderHistoryController);
--- public/modules/orders/controllers/order-history.client.controller.ts
+++ public/modules/orders/controllers/order-history.client.controller.ts
@@ -1,21 +1,33 @@
 'use strict';
 
 interface IOrderHistoryController {
+	amountPaid: (prices: Array<any>) => void;
 	cancel: () => void;
 }
 // Order history controller
 class OrderHistoryController implements IOrderHistoryController {
   static controllerId = 'OrderHistoryController';
 
-  static $inject = ['$scope','$uibModalInstance','order'];
+  static $inject = ['$scope','$uibModalInstance','order', '$filter'];
 
-  constructor(private $scope, private $uibModalInstance, private order) {
+  constructor(private $scope, private $uibModalInstance, private order, private $filter) {
   }
+
+	amountPaid(prices: Array<any>): void {
+		angular.forEach(prices, (price) => {
+			console.log(price, price.paid);
+			if(price.paid) {
+				var cost = this.$filter('number')(price.price, 2);
+				cost = this.$filter('currency')(price.price, '£');
+				return cost;
+			}
+		});
+	}
 
   cancel(): void {
     this.$uibModalInstance.dismiss('cancel');
   }
-	
+
 }
 //Register controller.
 angular.module('orders').controller(OrderHistoryController.controllerId, OrderHistoryController);

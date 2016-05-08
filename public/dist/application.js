0ffb002033c902c78c243b4340ecfbb718fd7d55
bakuzan <imannoyed4days@hotmail.co.uk>
2016-05-08 20:52:51 +0100

    view pages layout tweeks. built.

--- public/dist/application.js
+++ public/dist/application.js
@@ -2550,7 +2550,22 @@ angular.module('core').filter('dayFilter', function() {
         return dateArray[1] + suffix + ' ' + month + ' ' + $filter('date')(input, 'yyyy');
     }
   };
+})
+.filter('numberFixedLen', function () {
+  return function (n, len) {
+      var num = parseInt(n, 10);
+      len = parseInt(len, 10);
+      if (isNaN(num) || isNaN(len)) {
+          return n;
+      }
+      num = ''+num;
+      while (num.length < len) {
+          num = '0'+num;
+      }
+      return num;
+  };
 });
+
 'use strict';
 
 //Service to provide common access to features.

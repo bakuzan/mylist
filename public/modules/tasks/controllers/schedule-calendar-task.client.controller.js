186306b23c8ce8477c2f3014bb021f86abe002d0
swalsh <imannoyed4days@hotmail.co.uk>
2016-05-10 11:48:40 +0100

    added future checking to task calendar.

--- public/modules/tasks/controllers/schedule-calendar-task.client.controller.js
+++ public/modules/tasks/controllers/schedule-calendar-task.client.controller.js
@@ -4,17 +4,33 @@
 angular.module('tasks').controller('ScheduleCalendarTaskController', ['$scope', '$uibModalInstance', 'moment', 'data', 'ListService',
 	function($scope, $uibModalInstance, moment, data, ListService) {
     var ctrl = this;
+		ctrl.today = new Date();
     ctrl.date = new Date(data.date);
+		var timeDiff = Math.abs(ctrl.date.getTime() - ctrl.today.getTime());
+		ctrl.daysFromToday = Math.ceil(timeDiff / (1000 * 3600 * 24));
 		ctrl.day = ctrl.date.getDay() - 1;
 		ctrl.days = data.days;
     ctrl.events = [];
-		console.log('data: ', data, '$scope', $scope);
+		// console.log('data: ', data, '$scope', $scope);
 
 		ctrl.init = function() {
 			var weekEnds = new Date(ListService.weekEndingForDate(ctrl.date));
 			angular.forEach(data.events, function(event) {
 				if(new Date(event.date) < weekEnds && ((event.day.substring(0, 3) === ctrl.days[ctrl.day].text) || (event.day === 'Any'))) {
-					ctrl.events.push(event);
+					// console.log('daysFromToday: ', ctrl.daysFromToday);
+					if(!event.daily) {
+						//in weeks
+						// console.log('weeks: ', event.completeTimes, Math.floor(ctrl.daysFromToday / 7), event.repeat);
+						if((event.completeTimes + Math.floor(ctrl.daysFromToday / 7) <= event.repeat) || event.repeat === 0) {
+							ctrl.events.push(event);
+						}
+					} else if(event.daily) {
+						//in days
+						// console.log('days: ', event.completeTimes, ctrl.daysFromToday, event.repeat);
+						if(((event.completeTimes + ctrl.daysFromToday) <= event.repeat) || event.repeat === 0) {
+							ctrl.events.push(event);
+						}
+					}
 				}
 			});
 			ctrl.events.sort(function(a, b) {

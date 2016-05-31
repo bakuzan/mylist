ae284d2047952853ebcd0cb4307982a11e4ed06c
swalsh <imannoyed4days@hotmail.co.uk>
2016-05-31 16:38:13 +0100

    ratings converted to ctrl as.

--- public/modules/ratings/controllers/ratings.client.controller.js
+++ public/modules/ratings/controllers/ratings.client.controller.js
@@ -3,110 +3,98 @@
 // History controller
 angular.module('ratings').controller('RatingsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Animeitems', 'Mangaitems', 'ListService', 'NotificationFactory', 'StatisticsService', 'spinnerService',
 	function($scope, $stateParams, $location, Authentication, Animeitems, Mangaitems, ListService, NotificationFactory, StatisticsService, spinnerService) {
-		$scope.authentication = Authentication;
+		var ctrl = this;
+		ctrl.authentication = Authentication;
+    ctrl.view = 'Anime';
+    //paging variables.
+    ctrl.pageConfig = {
+        currentPage: 0,
+        pageSize: 20
+    };
+		ctrl.modelOptions = { debounce: 700 };
+    ctrl.sortType = 'rating';
+    ctrl.sortReverse = true;
+    ctrl.viewItem = undefined;
+    ctrl.ratingLevel = undefined; //default rating filter
+    //rating 'tooltip' function
+    ctrl.maxRating = 10;
+    ctrl.hoveringOver = function(value) {
+        ctrl.overStar = value;
+        ctrl.percent = 100 * (value / ctrl.maxRating);
+    };
 
-        // If user is not signed in then redirect back to signin.
-		if (!$scope.authentication.user) $location.path('/signin');
+    ctrl.go = function(id) {
+        $location.path('/mangaitems/' + id);
+    };
 
-        $scope.go = function(id) {
-            $location.path('/mangaitems/' + id);
-        };
-
-        $scope.view = 'Anime';
-        //paging variables.
-        $scope.pageConfig = {
-            currentPage: 0,
-            pageSize: 20
-        };
-        $scope.sortType = 'rating';
-        $scope.sortReverse = true;
-        $scope.viewItem = undefined;
-        $scope.ratingLevel = undefined; //default rating filter
-        //rating 'tooltip' function
-        $scope.maxRating = 10;
-        $scope.hoveringOver = function(value) {
-            $scope.overStar = value;
-            $scope.percent = 100 * (value / $scope.maxRating);
-        };
-
-        function getItems(view) {
-            if (view === 'Anime') {
-                spinnerService.loading('rating', Animeitems.query().$promise.then(function(result) {
-                    $scope.items = result;
-                }));
-            } else if (view === 'Manga') {
-                spinnerService.loading('rating', Mangaitems.query().$promise.then(function(result) {
-                    $scope.items = result;
-                }));
-            }
-            $scope.viewItem = undefined;
-//            console.log(view, $scope.items);
+    function getItems(view) {
+        if (view === 'Anime') {
+            spinnerService.loading('rating', Animeitems.query().$promise.then(function(result) {
+                ctrl.items = result;
+            }));
+        } else if (view === 'Manga') {
+            spinnerService.loading('rating', Mangaitems.query().$promise.then(function(result) {
+                ctrl.items = result;
+            }));
         }
+        ctrl.viewItem = undefined;
+    }
 
-        $scope.find = function(view) {
-            getItems(view);
-        };
+    ctrl.find = function(view) {
+			if(view === 'Anime' || view === 'Manga') {
+				 getItems(view);
+			 } else {
+				ctrl.view = 'Anime';
+				getItems(ctrl.view);
+			 }
+    };
 
-        //Needed to catch 'Character' setting and skip it.
-        $scope.$watch('view', function(newValue) {
-            if ($scope.view !== undefined) {
-                $scope.isLoading = true;
-                if (newValue !== 'Anime' && newValue !== 'Manga') {
-                    $scope.view = 'Anime';
-                } else {
-                    getItems($scope.view);
-                }
-            }
-        });
+    //apply new score.
+    ctrl.itemScore = function(item, score) {
+        if (item.rating !== score) {
+            item.rating = score;
 
-        //apply new score.
-        $scope.itemScore = function(item, score) {
-            if (item.rating !== score) {
-                item.rating = score;
-
-                item.$update(function() {
-                    $location.path('ratings');
-                    NotificationFactory.success('Saved!', 'New rating was saved successfully');
-                }, function(errorResponse) {
-                    $scope.error = errorResponse.data.message;
-                    NotificationFactory.error('Error!', 'Your change failed!');
-                });
+            item.$update(function() {
+                $location.path('ratings');
+                NotificationFactory.success('Saved!', 'New rating was saved successfully');
+            }, function(errorResponse) {
+                ctrl.error = errorResponse.data.message;
+                NotificationFactory.error('Error!', 'Your change failed!');
+            });
 //                console.log('update');
-            }
-            return false;
-        };
+        }
+        return false;
+    };
 
-        /** Episode rating functions below here.
-         */
-        $scope.viewEpisodeRatings = function(item) {
-            $scope.viewItem = ($scope.viewItem !== item) ? item : undefined;
-            $scope.isEqual = ($scope.viewItem === item) ? true : false;
-            $scope.search = ($scope.viewItem === item) ? item.title : '';
-            if ($scope.viewItem !== undefined) {
-                spinnerService.loading('summary',
-                                       StatisticsService.buildSummaryFunctions($scope.viewItem.meta.history).then(function(result) {
-                    $scope.summaryFunctions = result;
+    /** Episode rating functions below here.
+     */
+    ctrl.viewEpisodeRatings = function(item) {
+        ctrl.viewItem = (ctrl.viewItem !== item) ? item : undefined;
+        ctrl.isEqual = (ctrl.viewItem === item) ? true : false;
+        ctrl.search = (ctrl.viewItem === item) ? item.title : '';
+        if (ctrl.viewItem !== undefined) {
+            spinnerService.loading('summary', StatisticsService.buildSummaryFunctions(ctrl.viewItem.meta.history).then(function(result) {
+                ctrl.summaryFunctions = result;
+            }));
+        }
+    };
+
+    ctrl.episodeScore = function(finished) {
+//            console.log('finished: ', finished, ctrl.viewItem.meta.history);
+        if (finished) {
+            var item = ctrl.viewItem;
+            item.$update(function() {
+                $location.path('ratings');
+                NotificationFactory.success('Saved!', 'New episode rating was saved successfully');
+                spinnerService.loading('summary', StatisticsService.buildSummaryFunctions(ctrl.viewItem.meta.history).then(function(result) {
+                    ctrl.summaryFunctions = result;
                 }));
-            }
-        };
-
-        $scope.episodeScore = function(finished) {
-//            console.log('finished: ', finished, $scope.viewItem.meta.history);
-            if (finished) {
-                var item = $scope.viewItem;
-                item.$update(function() {
-                    $location.path('ratings');
-                    NotificationFactory.success('Saved!', 'New episode rating was saved successfully');
-                    spinnerService.loading('summary',
-                                           StatisticsService.buildSummaryFunctions($scope.viewItem.meta.history).then(function(result) {
-                        $scope.summaryFunctions = result;
-                    }));
-                }, function(errorResponse) {
-                    $scope.error = errorResponse.data.message;
-                    NotificationFactory.error('Error!', 'Your change failed!');
-                });
-            }
-        };
+            }, function(errorResponse) {
+                ctrl.error = errorResponse.data.message;
+                NotificationFactory.error('Error!', 'Your change failed!');
+            });
+        }
+    };
 
 
     }

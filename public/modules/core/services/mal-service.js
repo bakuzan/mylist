1bd5e04027ff1a55feed3b226015034f8eac7eac
bakuzan <imannoyed4days@hotmail.co.uk>
2016-10-19 21:53:22 +0100

    added mal search query & xmlToJson()

---
+++ public/modules/core/services/mal-service.js
@@ -0,0 +1,80 @@
+(function() {
+	'use strict';
+	angular.module('core')
+	.factory('MalService', MalService);
+	MalService.$inject = ['$http'];
+
+	function MalService($http) {
+		var credentials = window.btoa('bakuzan:Myanime52');
+		$http.defaults.headers.common['Authorization'] = `Basic ${credentials}`;
+
+		function xmlToJson(xml) {
+			var ELEMENT_NODE_TYPE = 1,
+				TEXT_NODE_TYPE = 3,
+				obj = {};
+
+			if (xml.nodeType == ELEMENT_NODE_TYPE) {
+				if (xml.attributes.length > 0) {
+				obj['@attributes'] = {};
+					for (var j = 0; j < xml.attributes.length; j++) {
+						var attribute = xml.attributes.item(j);
+						obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
+					}
+				}
+			} else if (xml.nodeType == TEXT_NODE_TYPE) {
+				obj = xml.nodeValue;
+			}
+
+			if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === TEXT_NODE_TYPE) {
+				obj = xml.childNodes[0].nodeValue;
+			}
+			else if (xml.hasChildNodes()) {
+				for(var i = 0; i < xml.childNodes.length; i++) {
+					var item = xml.childNodes.item(i),
+						nodeName = item.nodeName;
+					if (typeof(obj[nodeName]) == 'undefined') {
+						obj[nodeName] = xmlToJson(item);
+					} else {
+						if (typeof(obj[nodeName].push) == 'undefined') {
+							var old = obj[nodeName];
+							obj[nodeName] = [];
+							obj[nodeName].push(old);
+						}
+						obj[nodeName].push(xmlToJson(item));
+					}
+				}
+			}
+			return obj;
+		}
+
+		function xmlProcessor(type, data) {
+			var searchResult = [],
+				xml = data.responseXML,
+				nodes = xml.evaluate(`//${type}/entry`, xml, null, XPathResult.ANY_TYPE, null),
+				result = nodes.iterateNext();
+			while (result) {
+				searchResult.push(xmlToJson(result));
+				result = nodes.iterateNext();
+			}
+			return searchResult;
+		}
+
+		return {
+			search: function(queryType, searchString) {
+				var searchUrl = `https://myanimelist.net/api/${queryType}/search.xml?q=${searchString}`;
+				$http({
+					method: 'GET', 
+					url: searchUrl
+				}).then(function success(result) {
+					console.log(`${searchUrl} >> ${result.data}`);
+					var processMalResponse = new Promise(function(resolve, reject) {
+						resolve(xmlProcessor(queryType, result.data));
+					});
+					return processMalResponse;
+				}, function error(result) {
+					console.log(`error searching mal at ${searchUrl}: ${result}`);
+				});
+			}
+		};
+	}
+})();

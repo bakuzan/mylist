1cf79a7fb227e2b475a8aa098683ff5ddd380401
bakuzan <imannoyed4days@hotmail.co.uk>
2016-11-04 09:17:57 +0000

    Create mal.server.routes.js

---
+++ app/routes/mal.server.routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(app) {
+	var users = require('../../app/controllers/users.server.controller');
+	var mal = require('../../app/controllers/mal.server.controller');
+
+	// MAL Routes
+	app.route('/malSearch/:type/:searchString')
+		.get(mal.search);
+};

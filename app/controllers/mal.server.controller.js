ca1ba790e509a1618e00360b2ca782aac6f87908
bakuzan <imannoyed4days@hotmail.co.uk>
2016-11-05 22:39:20 +0000

    adjust

--- app/controllers/mal.server.controller.js
+++ app/controllers/mal.server.controller.js
@@ -8,18 +8,26 @@ var mongoose = require('mongoose'),
 	_ = require('lodash'),
   popura = require('popura');
 
-const client = popura('Bakuzan', 'Myanime52');
+var client = popura('Bakuzan', 'Myanime52');
 
 exports.search = function(req, res) {
 	console.log('search mal: ', req);
 	//get params here
 	var type = '',
-		search = '';
-	
+			search = '';
+
 	// for now log the result, when got it working just return the query.
 	if(type === 'anime') {
-		client.searchAnimes(search).then(res => console.log(res)).catch(err => console.log(err));
+		client.searchAnimes(search).then(function(res) {
+			console.log(res);
+		}).catch(function(err) {
+			console.log(err);
+		});
 	} else if (type === 'manga') {
-		client.searchMangas(search).then(res => console.log(res)).catch(err => console.log(err));
+		client.searchMangas(search).then(function(res) {
+			console.log(res);
+		}).catch(function(err) {
+			 console.log(err);
+		});
 	}
 };

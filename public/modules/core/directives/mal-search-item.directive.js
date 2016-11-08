3ffa6759708c18d77854bb3ba82f0f1ef8d43a59
bakuzan <imannoyed4days@hotmail.co.uk>
2016-11-08 09:41:40 +0000

    Create mal-search-item.directive.js

---
+++ public/modules/core/directives/mal-search-item.directive.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+  angular.module('core')
+  .directive('malSearchItem', malSearchItem);
+  malSearchItem.$inject = ['$timeout'];
+
+  function malSearchItem($timeout) {
+      return {
+          restrict: 'A',
+          require: '^malSearch',
+          scope: {
+            item: '@malSearchItem'
+          },
+          link: function (scope, element, attrs) {
+
+          }
+      };
+  }
+
+})();

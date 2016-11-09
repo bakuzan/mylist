4a7b7cc39b8fb3601aa616bc5c8dfea54cff2526
bakuzan <imannoyed4days@hotmail.co.uk>
2016-11-09 22:08:26 +0000

    more actions and cosmetics

--- public/modules/core/directives/mal-search.directive.js
+++ public/modules/core/directives/mal-search.directive.js
@@ -18,17 +18,27 @@
           bindToController: true,
           templateUrl: '/modules/core/templates/mal-search.html',
           controller: function ($scope) {
-            var self = this;
+            var self = this,
+                timeoutPromise,
+                delayInMs = 2000;
 
+            self.displaySelectedItemActions = displaySelectedItemActions;
+            self.hasFocus = false;
             self.hasSearchResults = false;
             self.processItem = processItem;
             self.searchResults = [];
             self.selectedItem = null;
+            self.toggleSearchDropdownOnFocus = toggleSearchDropdownOnFocus;
             console.log('mal search scope: ', $scope);
+
             function processItem(item) {
               console.log('processItem :', item);
               self.selectItem(item);
               self.selectedItem = item;
+            }
+
+            function displaySelectedItemActions() {
+              console.log('display actions', self.selectedItem);
             }
 
             function searchMal(type, searchString) {
@@ -39,8 +49,10 @@
               });
             }
 
-            var timeoutPromise;
-            var delayInMs = 2000;
+            function toggleSearchDropdownOnFocus(event) {
+              self.hasFocus = true || event.type === 'focus'; //hard code true for dev
+            }
+
             $scope.$watch('malSearchCtrl.searchString', function(newValue) {
               if(newValue !== undefined && newValue.length > 2) {
                 $timeout.cancel(timeoutPromise);  //does nothing, if timeout alrdy done
